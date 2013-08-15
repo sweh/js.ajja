51893e65d074b1df1cdb0414abd43ecc1743b5f3
Sebastian Wehrmann <sw@gocept.com>
2013-08-15 15:35:20 +0200

    Add some more input fields.

--- src/gocept/jsform/resources/widgets.js
+++ src/gocept/jsform/resources/widgets.js
@@ -6,4 +6,20 @@ gocept.jsform.widgets.form = new jsontemplate.Template('\
 <form method="POST" action="." id="{form_id}">\
 </form>', {default_formatter: 'html',  undefined_str: ''});
 
+gocept.jsform.widgets.string = new jsontemplate.Template('\
+<input type="text" name="{name}" value="{value}" readonly="{readonly}" />\
+', {default_formatter: 'html',  undefined_str: ''});
+
+gocept.jsform.widgets.object = new jsontemplate.Template('\
+<select name="{name}" readonly="{readonly}">\
+  {.repeated section value}\
+    <option value="{id}">{value}</option>\
+  {.end}\
+</select>', {default_formatter: 'html',  undefined_str: ''});
+
+gocept.jsform.widgets.boolean = new jsontemplate.Template('\
+<input type="radio" name="{name}" value="yes">Yes</input><br />\
+<input type="radio" name="{name}" value="no">No</input>\
+', {default_formatter: 'html',  undefined_str: ''});
+
 }(jQuery));
