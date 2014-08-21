81b862a0a4a78016e5704b0709909f8b02b714ef
Sebastian Wehrmann <sw@gocept.com>
2014-08-21 13:45:23 +0200

    Repair imports so that tests nur mostly run.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,13 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.pt$/, loader: "html" }
+            { test: /\.pt$/, loader: "html" },
+            {
+                test: /json-template\.js/,
+                loader: "imports?exports=>{}!exports?exports"
+            },
+            { test: /ko\.mapping\.js/, loader: "imports?this=>{}" },
+            { test: /classy\.js/, loader: "imports?this=>{}" }
         ]
     }
 };
