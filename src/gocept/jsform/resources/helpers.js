4de2658271c682c2651ab82f08e853b9876d1cfb
Wolfgang Schnerring <ws@gocept.com>
2014-08-21 15:07:00 +0200

    Transform helpers.js to commonjs module style

--- src/gocept/jsform/resources/helpers.js
+++ src/gocept/jsform/resources/helpers.js
@@ -1,30 +1,31 @@
-(function($) {
+var $ = require('bower-jquery');
 
-  var isUndefinedOrNull = function(o) {
-    return typeof(o) == 'undefined' || o === null;
-  };
 
-  var declare_namespace = function(namespace) {
-    var obj = window;
-    $.each(namespace.split('.'), function(i, name) {
-      if (isUndefinedOrNull(obj[name])) {
-        obj[name] = {};
-      }
-      obj = obj[name];
-    });
-  };
+var isUndefinedOrNull = function(o) {
+  return typeof(o) == 'undefined' || o === null;
+};
 
-  declare_namespace('gocept.jsform');
-  gocept.jsform.declare_namespace = declare_namespace;
-  gocept.jsform.isUndefinedOrNull = isUndefinedOrNull;
-
-  gocept.jsform.or = function(value1, value2) {
-    if (!isUndefinedOrNull(value1)) {
-      return value1;
-    } else {
-      return value2;
+var declare_namespace = function(namespace) {
+  var obj = window;
+  $.each(namespace.split('.'), function(i, name) {
+    if (isUndefinedOrNull(obj[name])) {
+      obj[name] = {};
     }
-  };
+    obj = obj[name];
+  });
+};
 
-}(jQuery));
+declare_namespace('gocept.jsform');
+gocept.jsform.declare_namespace = declare_namespace;
+gocept.jsform.isUndefinedOrNull = isUndefinedOrNull;
 
+gocept.jsform.or = function(value1, value2) {
+  if (!isUndefinedOrNull(value1)) {
+    return value1;
+  } else {
+    return value2;
+  }
+};
+
+
+module.exports = gocept;
